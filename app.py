e8b0dfce90455d46b9e319bbd2534ee595fc7a16
Tiago <sl5371023@gmail.com>
2025-09-16 08:05:33 -0300

    aula 1

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+# rodar no CMD
+# pip install flask
 
+from flask import Flask
+
+# Cria uma instância da aplicação Flask
+app = Flask(__Name__)
+# Estte é um decorador que associa a url
+# '/' (a URL raiz do site) à função que vem logo a baixo.
+@app.route('/')
+# A função que é executada quando a rota '/' é acessada.
+# Ela retorna a string "hello world"
+def hello_world():
+    return 'Hello, world!'
+    # executa o servidor de desenvolvimento
+    if__name__=='__main__'
+    app.run(debug=True)
